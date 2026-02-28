aa42aecf39830f72ba658d008e1d222c9032c8ec
rohith <rohithkrishnan997@gmail.com>
2026-02-28 09:29:54 +0530

    Update db.ts

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 const connectDB = async():Promise <void> =>{
     try{
-        await mongoose.connect(process.env.MONGODB_URI as string)
+        await mongoose.connect(process.env.MONGODB_URI !)
         console.log("Mongodb connected")
     }catch(error){
         console.error("MongoDB connection error",error)
